6ca787c02085ed2a77f1563206d2fc71abb80aae
MartinBG@abv.bg <MartinBG@abv.bg>
2018-03-19 16:43:04 +0200

    Use Dynamic Programming for Binominal Coefficient calculation

--- src/_02_combinatorial_algorithms/exercises/Pr07NChooseKCount.java
+++ src/_02_combinatorial_algorithms/exercises/Pr07NChooseKCount.java
@@ -9,12 +9,28 @@ public class Pr07NChooseKCount {
         int n = Integer.parseInt(scanner.nextLine());
         int k = Integer.parseInt(scanner.nextLine());
 
-        long result = binom(n, k);
+        long result = binomialCoefficientDynamicProgramming(n, k);
 
         System.out.println(result);
     }
 
-    private static long binom(int n, int k) {
+    // https://www.geeksforgeeks.org/dynamic-programming-set-9-binomial-coefficient/
+    private static long binomialCoefficientDynamicProgramming(int n, int k) {
+        long[] dp = new long[k + 1];
+
+        dp[0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            // Compute next row of Pascal triangle using the previous row
+            for (int j = Math.min(i, k); j > 0; j--) {
+                dp[j] = dp[j] + dp[j - 1];
+            }
+        }
+
+        return dp[k];
+    }
+
+    private static long binomialCoefficientRecursion(int n, int k) {
         if (k > n) {
             return 0L;
         }
@@ -23,6 +39,6 @@ public class Pr07NChooseKCount {
             return 1L;
         }
 
-        return binom(n - 1, k - 1) + binom(n - 1, k);
+        return binomialCoefficientRecursion(n - 1, k - 1) + binomialCoefficientRecursion(n - 1, k);
     }
 }
