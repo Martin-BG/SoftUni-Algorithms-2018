3cdabc74172492a40a5f21cac500030ae1e5a1d3
MartinBG@abv.bg <MartinBG@abv.bg>
2018-03-26 15:32:30 +0300

    Lab

--- src/_02_combinatorial_algorithms/lab/Pr07NChooseKCountDynamicProgramming.java
+++ src/_02_combinatorial_algorithms/lab/Pr07NChooseKCountDynamicProgramming.java
@@ -1,8 +1,8 @@
-package _02_combinatorial_algorithms.exercises;
+package _02_combinatorial_algorithms.lab;
 
 import java.util.Scanner;
 
-public class Pr07NChooseKCount {
+public class Pr07NChooseKCountDynamicProgramming {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -28,17 +28,5 @@ public class Pr07NChooseKCount {
         }
 
         return dp[k];
-    }
-
-    private static long binomialCoefficientRecursion(int n, int k) {
-        if (k > n) {
-            return 0L;
-        }
-
-        if (k == 0 || k == n) {
-            return 1L;
-        }
-
-        return binomialCoefficientRecursion(n - 1, k - 1) + binomialCoefficientRecursion(n - 1, k);
     }
 }
