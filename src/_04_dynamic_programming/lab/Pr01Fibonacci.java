3f07a7d1b7cd169f714ecd06287d92da53768dad
MartinBG@abv.bg <MartinBG@abv.bg>
2018-04-10 11:38:54 +0300

    Refactored solutions

--- src/_04_dynamic_programming/lab/Pr01Fibonacci.java
+++ src/_04_dynamic_programming/lab/Pr01Fibonacci.java
@@ -3,26 +3,27 @@ package _04_dynamic_programming.lab;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 public class Pr01Fibonacci {
 
-    private static final Charset ENCODING = Charset.forName("UTF-8");
-
     public static void main(final String[] args) {
 
-        try (final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in, ENCODING))) {
+        try (final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8))) {
 
             final int n = Integer.parseInt(reader.readLine());
 
-            System.out.println(findFibonacci(n));
+            System.out.println(findFibonacciIterative(n));
+//            System.out.println(findFibonacciRecursiveWithMemoization(new long[n+1], n));
         } catch (IOException | NullPointerException | NumberFormatException e) {
             e.printStackTrace();
         }
     }
 
-    private static long findFibonacci(final int n) {
-        if (n <= 2) {
+    private static long findFibonacciIterative(final int n) {
+        if (n == 0) {
+            return 0L;
+        } else if (n <= 2) {
             return 1L;
         }
 
@@ -38,20 +39,21 @@ public class Pr01Fibonacci {
         return current;
     }
 
-    private static long findFibonacciWithMemoization(final int n) {
-        if (n <= 2) {
-            return 1L;
+    private static long findFibonacciRecursiveWithMemoization(final long[] memo, final int n) {
+        if (n <= 0) {
+            return 0;
+        } else if (n <= 2) {
+            memo[n] = 1;
         }
 
-        final long[] fibonacci = new long[n + 1];
-
-        fibonacci[0] = 0;
-        fibonacci[1] = 1;
-
-        for (int i = 2; i <= n; i++) {
-            fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+        if (memo[n] > 0) {
+            return memo[n];
         }
 
-        return fibonacci[n];
+        long current = findFibonacciRecursiveWithMemoization(memo, n - 1) + findFibonacciRecursiveWithMemoization(memo, n - 2);
+
+        memo[n] = current;
+
+        return current;
     }
 }
