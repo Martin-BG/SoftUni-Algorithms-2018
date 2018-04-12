b7654e57848e5b7c5df9cd5ca17260436f1c9e2d
MartinBG@abv.bg <MartinBG@abv.bg>
2018-04-12 17:25:13 +0300

    TODOs

---
+++ src/_04_dynamic_programming/exercises/Pr05SumWithLimitedCoins.java
@@ -0,0 +1,18 @@
+package _04_dynamic_programming.exercises;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+public class Pr05SumWithLimitedCoins {
+
+    public static void main(final String[] args) {
+
+        try (final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8))) {
+            // TODO
+        } catch (IOException | NullPointerException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/_05_dynamic_programming_part_2/exercises/Pr03SymbolMultiplication.java
@@ -0,0 +1,18 @@
+package _05_dynamic_programming_part_2.exercises;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+public class Pr03SymbolMultiplication {
+
+    public static void main(final String[] args) {
+
+        try (final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8))) {
+            // TODO
+        } catch (IOException | NullPointerException e) {
+            e.printStackTrace();
+        }
+    }
+}
