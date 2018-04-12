1ca3f3707896ad21730ad428574e106d3d01bad0
MartinBG@abv.bg <MartinBG@abv.bg>
2018-04-12 17:49:20 +0300

    Solution

--- src/_04_dynamic_programming/exercises/Pr04SumWithUnlimitedCoins.java
+++ src/_04_dynamic_programming/exercises/Pr04SumWithUnlimitedCoins.java
@@ -4,13 +4,35 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+// https://www.geeksforgeeks.org/dynamic-programming-set-7-coin-change/
 
 public class Pr04SumWithUnlimitedCoins {
 
     public static void main(final String[] args) {
 
         try (final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8))) {
-            // TODO
+            int[] coins = Arrays
+                    .stream(reader.readLine().trim().split("\\s+"))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+
+            int targetSum = Integer.parseInt(reader.readLine());
+
+            int[] combinationsCount = new int[targetSum + 1];
+            combinationsCount[0] = 1;   // targetSum = 0
+
+            for (final int coin : coins) {
+                for (int sum = 1; sum <= targetSum; sum++) {
+                    if (coin <= sum) {
+                        combinationsCount[sum] += combinationsCount[sum - coin];
+                    }
+                }
+            }
+
+            System.out.println(combinationsCount[targetSum]);
+
         } catch (IOException | NullPointerException e) {
             e.printStackTrace();
         }
