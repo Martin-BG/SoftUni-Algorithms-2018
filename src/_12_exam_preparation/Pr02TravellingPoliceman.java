68fd9a9c106bc74e1142b6a0a5af5eb0b7c2badb
MartinBG <MartinBG@abv.bg>
2018-05-09 05:07:46 +0300

    Refactoring

--- src/_12_exam_preparation/Pr02TravellingPoliceman.java
+++ src/_12_exam_preparation/Pr02TravellingPoliceman.java
@@ -4,7 +4,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
 
 public class Pr02TravellingPoliceman {
 
@@ -12,27 +15,15 @@ public class Pr02TravellingPoliceman {
 
         final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
 
-        int fuel = Integer.parseInt(reader.readLine());
+        final int fuel = Integer.parseInt(reader.readLine());
+        final List<Street> streets = readStreets(reader, fuel);
 
-        final List<Street> streets = new ArrayList<>();
-        String line;
-        while (!"End".equals(line = reader.readLine())) {
-            String[] tokens = line.split(", ");
+        final Deque<Street> visited = findPath(streets, fuel);
 
-            String name = tokens[0];
-            int damage = Integer.parseInt(tokens[1]);
-            int pokemonCount = Integer.parseInt(tokens[2]);
-            int length = Integer.parseInt(tokens[3]);
+        printPath(visited, fuel);
+    }
 
-            final Street street = new Street(name, damage, pokemonCount, length);
-
-            if (street.value <= 0 || street.length > fuel) {
-                continue;
-            }
-
-            streets.add(street);
-        }
-
+    private static Deque<Street> findPath(final List<Street> streets, final int fuel) {
         final int streetsCount = streets.size();
 
         final int[][] dp = new int[streetsCount + 1][fuel + 1];
@@ -42,13 +33,15 @@ public class Pr02TravellingPoliceman {
             System.arraycopy(dp[row - 1], 0, dp[row], 0, street.length);
 
             for (int col = street.length; col <= fuel; col++) {
-                dp[row][col] = Math.max(dp[row - 1][col], dp[row - 1][col - street.length] + street.value);
+                dp[row][col] = Math.max(
+                        dp[row - 1][col],
+                        dp[row - 1][col - street.length] + street.value);
             }
 
             row++;
         }
 
-        final Set<Street> visited = new TreeSet<>(Comparator.comparingInt(street -> street.index));
+        final Deque<Street> visited = new ArrayDeque<>();
 
         row = streetsCount;
         int col = fuel;
@@ -61,10 +54,14 @@ public class Pr02TravellingPoliceman {
                 row--;
                 final Street street = streets.get(row);
                 col -= street.length;
-                visited.add(street);
+                visited.addFirst(street);
             }
         }
 
+        return visited;
+    }
+
+    private static void printPath(final Deque<Street> visited, final int fuel) {
         int pokemonCaught = 0;
         int totalCarDamage = 0;
         int fuelLeft = fuel;
@@ -92,31 +89,55 @@ public class Pr02TravellingPoliceman {
         System.out.println(sb);
     }
 
+    private static List<Street> readStreets(final BufferedReader reader, final int fuel) throws IOException {
+        final List<Street> streets = new ArrayList<>();
+
+        String line;
+        while (!"End".equals(line = reader.readLine())) {
+            String[] tokens = line.split(", ");
+
+            String name = tokens[0];
+            int damage = Integer.parseInt(tokens[1]);
+            int pokemonCount = Integer.parseInt(tokens[2]);
+            int length = Integer.parseInt(tokens[3]);
+
+            final Street street = new Street(name, damage, pokemonCount, length);
+
+            if (street.value <= 0 || street.length > fuel) {
+                continue;
+            }
+
+            streets.add(street);
+        }
+
+        return streets;
+    }
+
     private static class Street {
         private static final int POKEMON_VALUE = 10;
 
-        private static int counter = 0;
-
-        private String name;
-        private int damage;
-        private int pokemonCount;
-        private int length;
+        private final String name;
+        private final int damage;
+        private final int pokemonCount;
+        private final int length;
         private int value;
-        private int index;
 
         Street(final String name, final int damage, final int pokemonCount, final int length) {
             this.name = name;
             this.damage = damage;
             this.pokemonCount = pokemonCount;
             this.length = length;
+            this.setValue();
+        }
+
+        private void setValue() {
             this.value = this.pokemonCount * POKEMON_VALUE - this.damage;
-            this.index = counter++;
         }
 
         @Override
         public String toString() {
-            return String.format("Street{name='%s', damage=%d, pokemonCount=%d, length=%d, value=%d, index=%d}",
-                    this.name, this.damage, this.pokemonCount, this.length, this.value, this.index);
+            return String.format("Street{name='%s', damage=%d, pokemonCount=%d, length=%d, value=%d}",
+                    this.name, this.damage, this.pokemonCount, this.length, this.value);
         }
     }
 }
